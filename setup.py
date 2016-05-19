58e57da5b3114c1284516261346c1f545a91ad53
ZodiacFireworks <martin.vuelta@gmail.com>
2016-05-19 09:38:47 -0500

    Django Geolocation
    Change log:
      * New setup files
      * Added MANIFEST
      * Added content to AUTHORS file
    
    Modified files:
      * modified:   AUTHORS
      * new file:   MANIFEST.in
      * modified:   setup.cfg
      * modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 from setuptools import setup, find_packages
 
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='django-geolocation',
+
     version='0.0.0',
+
     description='A collection of Django utilities to store simple geographical information.',
+    long_description=long_description,
+
     author='Martin Vuelta',
     author_email='martin.vuelta@gmail.com',
+
+    license='MIT',
+
     url='https://github.com/ZodiacFireworks/django-geolocation',
     download_url='https://github.com/ZodiacFireworks/django-geolocation/tarball/0.0.0',
-    keywords=['django', 'geographical information', 'geoposition', 'geolocation'],
-    packages=find_packages(),
-    zip_safe=True,
-    package_data={
-        'geolocation': [
-        ],
-    },
+
     classifiers=[
         'Development Status :: 1 - Planning',
         'Environment :: Web Environment',
@@ -24,5 +34,24 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    install_requires=['django-appconf >= 0.4'],
+
+    keywords=['django', 'geographical information', 'geoposition', 'geolocation'],
+
+    zip_safe=True,
+    packages=find_packages(
+        exclude=[
+            'contrib',
+            'docs',
+            'tests'
+        ]
+    ),
+
+    package_data={
+        'geolocation': [
+        ],
+    },
+
+    install_requires=[
+        'django-appconf >= 0.4',
+    ],
 )
